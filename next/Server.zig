waycrate/NextWM

commit 6c37f772af7e0265a42ff118208d8479978d2548

[next] Fix memory leak due to unfree'd arraylists
Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

next/Server.zig
@@ -193,6 +193,9 @@ fn terminateCb(_: c_int, wl_server: *wl.Server) callconv(.C) c_int {
 
 pub fn deinit(self: *Self) void {
     self.windows.deinit(allocator);
+    self.outputs.deinit(allocator);
+    self.keyboards.deinit(allocator);
+    self.cursors.deinit(allocator);
 
     self.sigabrt_cb.remove();
     self.sigint_cb.remove();
@@ -222,6 +225,7 @@ fn newOutput(_: *wl.Listener(*wlr.Output), wlr_output: *wlr.Output) void {
         std.log.err("Failed to allocate new output", .{});
         return;
     };
+    errdefer allocator.free(output);
 
     // Instantiate the output struct.
     output.init(wlr_output);
@@ -244,6 +248,7 @@ fn newXdgSurface(listener: *wl.Listener(*wlr.XdgSurface), xdg_surface: *wlr.XdgS
             std.log.err("Failed to allocate new view", .{});
             return;
         };
+        errdefer allocator.free(window);
 
         // Populating the window struct with it's members.
         window.* = .{

commit faa879132399d2765910e219a5ed2164dc32fb99

[next] Fix regression introduced in 6c37f772af7e0265a42ff118208d8479978d2548
Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

next/Server.zig
@@ -192,11 +192,6 @@ fn terminateCb(_: c_int, wl_server: *wl.Server) callconv(.C) c_int {
 }
 
 pub fn deinit(self: *Self) void {
-    self.windows.deinit(allocator);
-    self.outputs.deinit(allocator);
-    self.keyboards.deinit(allocator);
-    self.cursors.deinit(allocator);
-
     self.sigabrt_cb.remove();
     self.sigint_cb.remove();
     self.sigquit_cb.remove();
@@ -208,6 +203,11 @@ pub fn deinit(self: *Self) void {
     self.wlr_backend.destroy();
     self.wlr_renderer.destroy();
     self.wlr_allocator.destroy();
+
+    self.windows.deinit(allocator);
+    self.outputs.deinit(allocator);
+    self.cursors.deinit(allocator);
+    self.keyboards.deinit(allocator);
 
     self.wlr_cursor.destroy();
     self.wlr_xcursor_manager.destroy();