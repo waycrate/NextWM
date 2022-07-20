711a240b537f1b377480125bc14a4329fec04102
Shinyzenith <aakashsensharma@gmail.com>
2022-07-20 14:52:36 +0530

    [next/Keyboard] Cleanup listener names

--- next/input/Keyboard.zig
+++ next/input/Keyboard.zig
@@ -24,8 +24,10 @@ server: *Server,
 wlr_input_device: *wlr.InputDevice,
 wlr_keyboard: *wlr.Keyboard,
 
-keyboard_key: wl.Listener(*wlr.Keyboard.event.Key) = wl.Listener(*wlr.Keyboard.event.Key).init(keyboardKey),
-keyboard_destroy: wl.Listener(*wlr.InputDevice) = wl.Listener(*wlr.InputDevice).init(keyboardDestroy),
+key: wl.Listener(*wlr.Keyboard.event.Key) = wl.Listener(*wlr.Keyboard.event.Key).init(handleKey),
+modifiers: wl.Listener(*wlr.Keyboard) = wl.Listener(*wlr.Keyboard).init(handleModifiers),
+
+destroy: wl.Listener(*wlr.InputDevice) = wl.Listener(*wlr.InputDevice).init(handleDestroy),
 
 pub fn init(self: *Self, device: *wlr.InputDevice) void {
     log.debug("Initializing keyboard device", .{});
@@ -41,12 +43,12 @@ pub fn init(self: *Self, device: *wlr.InputDevice) void {
 
     self.wlr_keyboard.setRepeatInfo(self.server.config.repeat_rate, self.server.config.repeat_delay);
 
-    self.wlr_keyboard.events.key.add(&self.keyboard_key);
-    self.wlr_input_device.events.destroy.add(&self.keyboard_destroy);
+    self.wlr_keyboard.events.key.add(&self.key);
+    self.wlr_input_device.events.destroy.add(&self.destroy);
 }
 
-fn keyboardKey(listener: *wl.Listener(*wlr.Keyboard.event.Key), event: *wlr.Keyboard.event.Key) void {
-    const self = @fieldParentPtr(Self, "keyboard_key", listener);
+fn handleKey(listener: *wl.Listener(*wlr.Keyboard.event.Key), event: *wlr.Keyboard.event.Key) void {
+    const self = @fieldParentPtr(Self, "key", listener);
     log.debug("Signal: wlr_keyboard_key", .{});
 
     self.server.input_manager.wlr_idle.notifyActivity(self.server.wlr_seat);
@@ -73,6 +75,24 @@ fn keyboardKey(listener: *wl.Listener(*wlr.Keyboard.event.Key), event: *wlr.Keyb
     self.server.wlr_seat.keyboardNotifyKey(event.time_msec, event.keycode, event.state);
 }
 
+fn handleModifiers(listener: *wl.Listener(*wlr.Keyboard), _: *wlr.Keyboard) void {
+    const self = @fieldParentPtr(Self, "modifiers", listener);
+
+    self.server.wlr_seat.setKeyboard(self.wlr_input_device);
+    self.server.wlr_seat.keyboardNotifyModifiers(&self.wlr_input_device.device.keyboard.modifiers);
+}
+
+fn handleDestroy(listener: *wl.Listener(*wlr.InputDevice), _: *wlr.InputDevice) void {
+    const self = @fieldParentPtr(Self, "destroy", listener);
+    log.debug("Signal: wlr_input_device_destroy (keyboard)", .{});
+
+    if (std.mem.indexOfScalar(*Self, self.server.keyboards.items, self)) |i| {
+        _ = self.server.keyboards.orderedRemove(i);
+    }
+
+    self.server.input_manager.setSeatCapabilities();
+}
+
 fn handleCompositorBindings(keysym: xkb.Keysym) bool {
     switch (@enumToInt(keysym)) {
         xkb.Keysym.XF86Switch_VT_1...xkb.Keysym.XF86Switch_VT_12 => {
@@ -87,15 +107,4 @@ fn handleCompositorBindings(keysym: xkb.Keysym) bool {
         },
         else => return false,
     }
-}
-
-fn keyboardDestroy(listener: *wl.Listener(*wlr.InputDevice), _: *wlr.InputDevice) void {
-    const self = @fieldParentPtr(Self, "keyboard_destroy", listener);
-    log.debug("Signal: wlr_input_device_destroy (keyboard)", .{});
-
-    if (std.mem.indexOfScalar(*Self, self.server.keyboards.items, self)) |i| {
-        _ = self.server.keyboards.orderedRemove(i);
-    }
-
-    self.server.input_manager.setSeatCapabilities();
 }
