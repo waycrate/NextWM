waycrate/NextWM

commit 96afa5440cab86ed7709ddf352ed70ba3fe196d4

[next/Xwayland] Flag to configure xwayland_lazy
Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

build.zig
@@ -40,9 +40,15 @@ pub fn build(builder: *std.build.Builder) !void {
     // Version information.
     const version = "0.1.0";
 
+    // Xwayland Lazy.
+    const xwayland_lazy = builder.option(bool, "xwayland_lazy", "Set to true to enable XwaylandLazy initialization") orelse false;
+
     // Create build options.
     const options = builder.addOptions();
+
+    // Adding build options which we can access in our source code.
     options.addOption([]const u8, "version", version);
+    options.addOption(bool, "xwayland_lazy", xwayland_lazy);
 
     // This block keeps the zig compositor version and the nextctl.h file version in sync.
     {

Makefile
@@ -1,6 +1,6 @@
 PREFIX=/usr
 BINDIR=$(PREFIX)/bin
-BUILD_FLAGS =
+BUILD_FLAGS = -Dxwayland_lazy
 # ^^^ Possible options:
 # -Drelease-safe
 # -Drelease-fast

next/Server.zig
@@ -8,6 +8,7 @@
 const Self = @This();
 
 const allocator = @import("./utils/allocator.zig").allocator;
+const build_options = @import("build_options");
 const c = @import("./utils/c.zig");
 const log = std.log.scoped(.Server);
 const std = @import("std");
@@ -141,8 +142,7 @@ pub fn init(self: *Self) !void {
     try self.seat.init();
 
     // Initializing Xwayland.
-    // True here indicates that Xwayland will be launched on demand.
-    self.wlr_xwayland = try wlr.Xwayland.create(self.wl_server, self.wlr_compositor, true);
+    self.wlr_xwayland = try wlr.Xwayland.create(self.wl_server, self.wlr_compositor, build_options.xwayland_lazy);
     self.wlr_xwayland.setSeat(self.seat.wlr_seat);
 
     // Initialize wl_shm, linux-dmabuf and other buffer factory protocols.

commit 12d55ba5c74928520793f08cfbec37201419a921

[next/XwaylandLazy] Fix regression introduced in 96afa5440cab86ed7709ddf352ed70ba3fe196d4
* We always defaulted to non-lazy xwayland due to a typo :)

Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

build.zig
@@ -41,7 +41,7 @@ pub fn build(builder: *std.build.Builder) !void {
     const version = "0.1.0";
 
     // Xwayland Lazy.
-    const xwayland_lazy = builder.option(bool, "xwayland_lazy", "Set to true to enable XwaylandLazy initialization") orelse false;
+    const xwayland_lazy = builder.option(bool, "xwayland_lazy", "Set to true to enable XwaylandLazy initialization") orelse true;
 
     // Create build options.
     const options = builder.addOptions();