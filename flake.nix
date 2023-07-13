47725932e651e1ec01be93492cdab20cfde15283
Shinyzenith <aakashsensharma@gmail.com>
2023-07-14 00:37:00 +0530

    [feat] Nixify the repo
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "NextWM devel";
+
+  inputs = {
+    nixgl.url = "github:guibou/nixGL";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixgl, ... }:
+    let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ nixgl.overlay ];
+        };
+
+      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs targetSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            name = "NextWM-devel";
+            nativeBuildInputs = with pkgs; [
+              # Compilers
+              cargo
+              clang
+              go
+              rustc
+              scdoc
+              zig
+
+              # Libs
+              libGL
+              libevdev
+              libinput
+              libxkbcommon
+              pixman
+              wayland
+              wayland-protocols
+              wlroots_0_16
+
+              # Tools
+              gdb
+              rustfmt
+              gnumake
+              gopls
+              pkg-config
+              pkgs.nixgl.nixGLMesa # For non-NixOS users who are using the nix package manager.
+              rust-analyzer
+              strace
+              valgrind
+              wayland-scanner
+              zls
+            ];
+          };
+        });
+    };
+}
