fdede9e2bce4ddc8834602e946be611b8abb6d1c
Shinyzenith <aakashsensharma@gmail.com>
2023-08-13 15:49:50 +0530

    [nextctl-c] Explicitly void cast un-handled returns
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

--- nextctl/src/nextctl.c
+++ nextctl/src/nextctl.c
@@ -11,10 +11,10 @@
 int main(int argc, char *argv[]) {
 	for (int i = 0; i < argc; i++) {
 		if (strcmp("-h", argv[i]) == 0 || strcmp("--help", argv[i]) == 0) {
-			fputs(usage, stderr);
+			(void)fputs(usage, stderr);
 			return EXIT_SUCCESS;
 		} else if (strcmp("-v", argv[i]) == 0 || strcmp("--version", argv[i]) == 0) {
-			printf("Nextctl version: %s\n", VERSION);
+			(void)printf("Nextctl version: %s\n", VERSION);
 			return EXIT_SUCCESS;
 		}
 	}
@@ -23,33 +23,33 @@ int main(int argc, char *argv[]) {
 	state.wl_display = wl_display_connect(NULL);
 
 	if (state.wl_display == NULL) {
-		fputs("ERROR: Cannot connect to wayland display.\n", stderr);
+		(void)fputs("ERROR: Cannot connect to wayland display.\n", stderr);
 		return EXIT_FAILURE;
 	}
 
 	state.wl_registry = wl_display_get_registry(state.wl_display);
-	wl_registry_add_listener(state.wl_registry, &registry_listener, &state);
+	(void)wl_registry_add_listener(state.wl_registry, &registry_listener, &state);
 
 	if (wl_display_dispatch(state.wl_display) < 0) {
-		fputs("ERROR: wayland dispatch failed.\n", stderr);
+		(void)fputs("ERROR: wayland dispatch failed.\n", stderr);
 		return EXIT_FAILURE;
 	}
 
 	if (state.next_control == NULL) {
-		fputs("ERROR: Compositor doesn't implement NextControlV1.\n", stderr);
+		(void)fputs("ERROR: Compositor doesn't implement NextControlV1.\n", stderr);
 		return EXIT_FAILURE;
 	}
 
 	for (int i = 1; i < argc; i++) {
-		next_control_v1_add_argument(state.next_control, argv[i]);
+		(void)next_control_v1_add_argument(state.next_control, argv[i]);
 	}
 
 	state.next_command_callback = next_control_v1_run_command(state.next_control);
-	next_command_callback_v1_add_listener(state.next_command_callback, &callback_listener,
-										  NULL);
+	(void)next_command_callback_v1_add_listener(state.next_command_callback,
+												&callback_listener, NULL);
 
 	if (wl_display_dispatch(state.wl_display) < 0) {
-		fputs("ERROR: wayland dispatch failed.\n", stderr);
+		(void)fputs("ERROR: wayland dispatch failed.\n", stderr);
 		return EXIT_FAILURE;
 	}
 
@@ -69,14 +69,14 @@ static void registry_handle_global(void *data, struct wl_registry *registry,
 
 static void next_handle_success(void *data, struct next_command_callback_v1 *callback,
 								const char *output) {
-	fputs(output, stdout);
+	(void)fputs(output, stdout);
 }
 
 static void next_handle_failure(void *data, struct next_command_callback_v1 *callback,
 								const char *failure_message) {
-	fprintf(stderr, "ERROR: %s", failure_message);
+	(void)fprintf(stderr, "ERROR: %s", failure_message);
 	if (strcmp("Unknown command\n\0", failure_message) == 0 ||
 		strcmp("No command provided\n\0", failure_message) == 0) {
-		fputs(usage, stderr);
+		(void)fputs(usage, stderr);
 	}
 }
